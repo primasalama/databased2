60a75a2921074aac65092f32d46f8ba9bc52ee3e
primasalama <inbox.prima@gmail.com>
2017-12-04 10:47:32 +0700

    slideshow

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,63 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<link href="http://olinamasi.diploma.ipb.ac.id/assets/css/full-slider.css" rel="stylesheet">
+<style type="text/css">
 
+    .parallax { 
+        /* The image used */
+        background-image: url("http://olinamasi.diploma.ipb.ac.id/assets/images/diploma1.jpg");
+
+        /* Set a specific height */
+        height: 830px; 
+        /* Create the parallax scrolling effect */
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        color:white;
+    }
+    
+    .style2 {
+        color: #FF9900
+    }
+    
+    .img-center {
+        margin: 0 auto;
+    }
+    .content{
+        padding-top: 90px;
+        padding-bottom: 90px;
+    }
+
+</style>
 
     <!-- Section: intro -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('http://olinamasi.diploma.ipb.ac.id/assets/images/ipb2.jpg');"></div>
+                <div class="carousel-caption" style="padding-bottom: 100px;">
+                    <img class="img-responsive img-center" style="width: 150px;" src="http://olinamasi.diploma.ipb.ac.id/assets/images/logo.png"></img>
+                    <h1><b>OLINAMASI 2017</b></h1>
+                    <h2 class="">Program Diploma IPB</h2>
+                    <p>"Berkarya Bersama, Berprestasi untuk Bangsa"</p>
+                    <h2><a href="http://olinamasi.diploma.ipb.ac.id/Register" class="btn btn-primary">Daftarkan Tim</a></h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('http://olinamasi.diploma.ipb.ac.id/assets/images/diploma.jpg');"></div>
+                <div class="carousel-caption" style="padding-bottom: 100px;">
+                    <img class="img-responsive img-center" style="width: 150px;" src="http://olinamasi.diploma.ipb.ac.id/assets/images/logo.png"></img>
+                    <h1><b>OLINAMASI 2017</b></h1>
+                    <h2 class="">Program Diploma IPB</h2>
+                    <p>7 Agustus - 13 Oktober 2017</p>
+                    <h2><a href="http://olinamasi.diploma.ipb.ac.id/#jadwal" class="btn btn-primary">Lihat Jadwal</a></h2>
+                </div>
+            </div>
+        </div>
     <!-- /Section: intro -->
 
     <!-- Section: boxes -->
@@ -73,5 +127,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- /Section: works -->
     <!-- Section: pricing -->
     <!-- /Section: pricing -->
-
-
+<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+    </script>
